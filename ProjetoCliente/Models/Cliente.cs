0db426eeb08bbf66c322741dfa09416d040cc683
Miguelpds10 <10072006miguel@gmail.com>
2024-05-23 15:45:54 -0300

    Criando estruttura cliente

---
+++ ProjetoCliente/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿namespace ProjetoCliente.Models
+{
+    public class Cliente
+    {
+        //Criando encapsulamento do objeto com get e set
+        public int Id { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? Telefone { get; set; }
+        public string? Email { get; set; }
+
+        public List<Cliente>? ListaCliente { get; set; }
+
+        
+        
+    }
+}
